888412317158ee9732e34d7fa2aebdeaea3800ea
Lukáš Kaňka <124378142+LukasKanka@users.noreply.github.com>
2023-08-17 13:26:38 +0200

    POM

---
+++ Selenium_POM_tutorial/readme.md
@@ -0,0 +1,14 @@
+https://www.youtube.com/watch?v=xVlSLhB3VcA&list=PLrBBHmoBFxBWV26aLM7VHcpljfJLHd6eQ&index=1
+
+Maven:
+
+'<properties>
+            <maven.compiler.source>20</maven.compiler.source>
+            <maven.compiler.target>20</maven.compiler.target>
+  </properties>'
+
+TestNG
+
+Selenium java
+
+WebDriverManager 
---
+++ Selenium_POM_tutorial/src/test/java/tutorial/POM/tests/Test_02_Login_POM.java
@@ -0,0 +1,47 @@
+package tutorial.POM.tests;
+
+import POM.pages._02.HomePage;
+import POM.pages._02.LoginPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Test_02_Login_POM {
+    WebDriver driver;
+   @BeforeMethod
+    // open website
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void test_Login_Functionality() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.fill_Textbox_username("standard_user");
+        loginPage.fill_Textbox_password("secret_sauce");
+        loginPage.click_Btn_Login();
+
+        HomePage homePage = new HomePage(driver);
+        String actual_msg = homePage.get_Msg_footer().getText();
+        String expected_msg = " Sauce Labs. All Rights Reserved. Terms of Service | Privacy Policy";
+
+        Assert.assertTrue(actual_msg.contains(expected_msg));
+
+    }
+
+    // close browser
+    @AfterMethod
+    public void teardown() {
+       driver.quit();
+    }
+
+
+}
