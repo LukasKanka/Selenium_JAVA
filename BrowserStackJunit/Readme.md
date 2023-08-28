080c51a003f7fb418478314ff3685801a54b4b56
Lukáš Kaňka <124378142+LukasKanka@users.noreply.github.com>
2023-08-28 22:37:49 +0200

    gitea + delete Browsestack

--- BrowserStackJunit/Readme.md
+++
@@ -1,7 +0,0 @@
-Install:
-
-Maven build
-
-Selenide: https://selenide.org/quick-start.html
-
-Junit5: search Juni t5 --> install Hunit Jupiter API 
--- lukanSelenide/src/test/java/cz/lukan/pages/HomePage.java
+++ lukanSelenide/src/test/java/cz/lukan/pages/HomePage.java
@@ -8,11 +8,13 @@ import static com.codeborne.selenide.Selenide.*;
 import static org.testng.AssertJUnit.assertEquals;
 
 public class HomePage {
-    public HomePage open() {
+    public HomePage open()
+    {
         Selenide.open("https://lukan.cz");
         return this;
     }
-    public HomePage titulek() {
+    public HomePage titulek()
+    {
          String title = title();
         assertEquals(title, "Lukáš bloguje - Blog o všem možném i nemožném");
         return this;
